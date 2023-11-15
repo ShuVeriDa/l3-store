f24028811ad9c654e6a432ce6d92d49dfc63dbdd
shuverida <bashtarov@outlook.com>
2023-11-15 14:23:30 +0300

    fixed bugs

--- src/modules/searchTips/searchTips.ts
+++ src/modules/searchTips/searchTips.ts
@@ -2,23 +2,13 @@ import html from "./searchTips.tpl.html";
 import {Component} from "../component";
 import {ProductData} from "../../../types";
 
-interface ITips {
-  name: string,
-  url: string
-}
-
 export class SearchTips extends Component {
   async render() {
     const res = await fetch('/api/getProducts');
     const products: ProductData[] = await res.json();
-    const tips = products.map(item => {
-     return {
-       name: item.name,
-       url: `product?id=${item.id}`
-     }
-    })
+    const tips = products.map(item => item.name)
 
-    const generateRandomEl = (tips: ITips[]) => {
+    const generateRandomEl = (tips: string[]) => {
       const randomIndex = Math.floor(Math.random() * tips.length);
       return tips[randomIndex];
     }
@@ -27,11 +17,9 @@ export class SearchTips extends Component {
       const tipsEls = document.querySelectorAll('.tip')
 
       tipsEls.forEach((item) => {
-        const obj  = generateRandomEl(tips)
-        const [first, second] = obj.name.split(' ')
-
+        const title  = generateRandomEl(tips)
+        const [first, second] = title.split(' ')
         item.textContent = `${first} ${second}`
-        item.setAttribute('href', obj.url)
       })
     }
 
